2a9f55fe6ca0cebe28aee678b001396bbf9ae3e8
MirrorsChen <1667809009@qq.com>
2019-11-05 18:30:23 +0800

    新增用例数据库表（列表+新增表）

--- atp3/apps/base/models.py
+++ atp3/apps/base/models.py
@@ -59,3 +59,14 @@ class serverinfo(models.Model):
         ordering = ["-c_time"]
         verbose_name = "服务器IP"
         verbose_name_plural = "服务器IP"
+
+class Product(models.Model):
+    productname = models.CharField('产品名称',max_length=64)  # 产品名称
+    productdesc = models.CharField('产品描述',max_length=200)    # 产品描述
+    producter = models.CharField('产品负责人',max_length=200,null=True)    # 产品负责人
+    create_time = models.DateTimeField('创建时间',auto_now=True)  # 创建时间-自动获取当前时间
+    class Meta:
+        verbose_name = '产品管理'
+        verbose_name_plural = '产品管理'
+    def __str__(self):
+        return self.productname
